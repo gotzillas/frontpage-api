14169378a2132d6e7523e3b55f68a434b5f4a11c
Tale Prestmo <tale.prestmo@knowit.no>
2019-03-15 08:47:52 +0100

    Remove schema version override

--- src/main/scala/no/ndla/frontpageapi/DBMigrator.scala
+++ src/main/scala/no/ndla/frontpageapi/DBMigrator.scala
@@ -15,7 +15,6 @@ object DBMigrator {
   def migrate(datasource: HikariDataSource): Int = {
     val flyway = Flyway
       .configure()
-      .table("schema_version") // Flyway's default table name changed, so we specify the old one.
       .dataSource(datasource)
       // Seems like flyway uses datasource.getConnection().getScheme() which is null if the scheme does not exist.
       // Therefore we simply override it with dataSource.getScheme.
